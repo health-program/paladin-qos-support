d6b858b2d74c976e30cff9ae28122f6c8f55b6a4
Tonto <823498927@qq.com>
2019-10-09 13:42:38 +0800

    bug fix

--- src/main/java/com/paladin/qos/analysis/impl/shejike/YiPay.java
+++ src/main/java/com/paladin/qos/analysis/impl/shejike/YiPay.java
@@ -3,12 +3,14 @@ package com.paladin.qos.analysis.impl.shejike;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.paladin.data.dynamic.SqlSessionContainer;
 import com.paladin.qos.analysis.DataProcessor;
 import com.paladin.qos.dynamic.DSConstant;
 import com.paladin.qos.dynamic.mapper.shejike.SheJiKeMapper;
 
+@Component
 public class YiPay extends DataProcessor{
 	@Autowired
 	private SqlSessionContainer sqlSessionContainer;
