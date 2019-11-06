0a05b6459e045b7658c6b873311905d1d104aa6e
Tonto <823498927@qq.com>
2019-11-06 09:24:55 +0800

    基础业务数据SQL保存

--- src/main/resources/sql/view.sql
+++
@@ -1,8 +0,0 @@
--- 门急诊就诊记录结构表（获取前31天内主要诊断疾病代码 主要疾病代码top5）
-select * from 
-(select substr(MD_DIS_CODE,0,5),ORGAN_CODE,count(1) from MedicalRecord
-where SEE_DOC_DT <= sysdate
-and SEE_DOC_DT>(sysdate-31)
-group by substr(MD_DIS_CODE,0,5),ORGAN_CODE
-order by count(1) desc)
-where rownum<6
